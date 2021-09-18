cf3d02175363448d6921410c6dbd37b7e5f7db53
WingsZeng <wings.xiangyi.zeng@gmail.com>
2021-09-18 16:37:49 +0800

    修改splay模板为蒋叶桢的, 并添加到pdf中

--- codes/data-struct/splay.cpp
+++ codes/data-struct/splay.cpp
@@ -1,101 +1,105 @@
-struct Node {
-	int val, ch[2], fa, size, cnt;
-} t[MAXN];
-int idx = 0, root = 0;
-/* 判断是否为右儿子 */
-int connect(int u) { return t[t[u].fa].ch[1] == u; }
-/* 向上更新节点 */
-void pushUp(int u) {
-	t[u].size = t[u].cnt + t[t[u].ch[0]].size + t[t[u].ch[1]].size;
+int data[MAXN];
+struct node *NIL;
+struct node {
+	int key, siz;
+	bool rev;
+	node *ch[2], *fa;
+	int dir() { return fa -> ch[1] == this; }
+	void setchild(node *x,int d) {
+		ch[d] = x;
+		x->fa = (x == NIL) ? NIL : this;
+	}
+	void PushUp() { siz = ch[0] -> siz + ch[1] -> siz + 1; }
+	void PushDown() {
+		if (!rev) return ;
+		swap(ch[0],ch[1]);
+		ch[0] -> rev ^= 1; ch[1] -> rev ^= 1;
+		rev=0;
+	}
+} tree[MAXN], *ncnt, *root;
+bool ok=0;
+void Init() {
+	ok = 0;
+	root = NIL = ncnt = &tree[0];
+	NIL -> key = -INF;
+	NIL -> siz = NIL -> rev = 0;
+	NIL -> fa = NIL -> ch[0] = NIL -> ch[1] = NIL;
 }
-/* 旋转, 把 zig 和 zag 写在了一起 */
-void rotate(int u) {
-	int f = t[u].fa, gf = t[f].fa, k = connect(u), s = t[u].ch[k^1];
-	t[gf].ch[connect(f)] = u, t[u].fa = gf;
-	t[f].ch[k] = s, t[s].fa = f;
-	t[u].ch[k^1] = f, t[f].fa = u;
-	pushUp(f), pushUp(u);
+node *Newnode(int val) {
+	node *p = ++ncnt;
+	p -> key = val, p -> siz = 1, p -> rev = 0;
+	p -> fa = p -> ch[0] = p -> ch[1] = NIL;
+	return p;
 }
-/* 将 u splay 为 goal 的儿子, goal=0 时 splay 到根 */
-void splay(int u, int goal = 0) {
-	while (t[u].fa != goal) {
-		int f = t[u].fa, gf = t[f].fa;
-		if (gf != goal) {
-			if (connect(u) == connect(f)) rotate(f);
-			else rotate(u);
+void Rotate(node *x) {
+	node *y = x -> fa;
+	int d = x -> dir();
+	if (y == root) root = x;
+	else y -> fa -> setchild(x, y -> dir());
+	x -> fa = y -> fa;
+	y -> setchild(x -> ch[!d], d);
+	x -> setchild(y, !d);
+	y -> PushUp();
+}
+void Splay(node *x, node *rt) {
+	node *y, *z;
+	x -> PushDown();
+	while (x -> fa != rt) {
+		y = x->fa; z = y -> fa;
+		if (z == rt) Rotate(x);
+		else {
+			if (x -> dir() == y->dir()) Rotate(y);
+			else Rotate(x);
+			Rotate(x);
 		}
-		rotate(u);
 	}
-	if (!goal) root = u;
+	x -> PushUp();
 }
-/* 将值为x的点splay到根, x不存在时会找到前驱或者后继 */
-int find(int x) {
-	int cur = root;
-	while (t[cur].val != x && t[cur].ch[x > t[cur].val])
-		cur = t[cur].ch[x > t[cur].val];
-	splay(cur);
-	return root;
-}
-/* 插入节点, 存在则cnt++, 不存在则新建, 并根据之前找到的f连边 */
-void insert(int x) {
-	int cur = root, f = 0;
-	while (x != t[cur].val && cur)
-		f = cur, cur = t[cur].ch[x > t[cur].val];
-	if (cur) t[cur].cnt++;
-	else {
-		cur = ++idx;
-		if (f) t[f].ch[x > t[f].val] = cur;
-		t[cur].ch[0] = t[cur].ch[1] = 0, t[cur].fa = f;
-		t[cur].cnt = t[cur].size = 1, t[cur].val = x;
+void Insert(node*rt, int val) {
+	if (rt == NIL) { rt = Newnode(val); return; }
+	node *x = rt;
+	int d = 0;
+	while (1) {
+		d = (val > x -> key);
+		if (x -> ch[d] == NIL) break;
+		x = x -> ch[d];
 	}
-	splay(cur);
+	node *y = Newnode(val);
+	x -> setchild(y, d);
+	x -> PushUp();
+	Splay(y, NIL);
 }
-/* 查找排名为k的值 */
-int getValueByRank(int k) {
-	int cur = root;
-	while(1) {
-		if (t[cur].ch[0] && k <= t[t[cur].ch[0]].size)
-			cur = t[cur].ch[0];
-		else if (k > t[cur].cnt + t[t[cur].ch[0]].size) {
-			//注意更改k
-			k -= t[cur].cnt + t[t[cur].ch[0]].size;
-			cur = t[cur].ch[1];
-		}
-		else return t[cur].val;
+node *Select(int k, node *f) {
+	node *p = root;
+	int lsz;
+	while (1) {
+		p -> PushDown();
+		lsz = p -> ch[0] -> siz;
+		if (k == lsz + 1) break;
+		if (k <= lsz) p = p -> ch[0];
+		else p = p -> ch[1], k = k - lsz - 1;
 	}
+	Splay(p, f);
+	return p;
 }
-/* 查找x的排名 */
-int getRankByValue(int x) { return t[t[find(x)].ch[0]].size + 1; }
-/* 查找x的前驱 */
-int getPre(int x) {
-	find(x);
-	if (x > t[root].val) return root;
-	int cur = t[root].ch[0];
-	while (t[cur].ch[1]) cur = t[cur].ch[1];
-	return cur;
+node *Maketree(node *fa, int l, int r) {
+	if (l > r) return NIL;
+	int mid = (l + r) >> 1;
+	node *p = Newnode(data[mid]);
+	p -> ch[0] = Maketree(p, l, mid - 1);
+	p -> ch[1] = Maketree(p, mid + 1, r);
+	p -> fa = fa;
+	p -> PushUp();
+	return p;
 }
-/* 查找x的后继 */
-int getSuc(int x) {
-	find(x);
-	if (x < t[root].val) return root;
-	int cur = t[root].ch[1];
-	while (t[cur].ch[0]) cur = t[cur].ch[0];
-	return cur;
-}
-/* 删除x */
-void erase(int x) {
-	//把前驱splay到根, 后继splay到根的右儿子, 那么右儿子的左儿子就是值为x的点
-	//由于pre和suc返回的是严格比x大/小的数
-	//所以如果不加入±∞会找到0点,splay(0)出错
-	int pre = getPre(x), suc = getSuc(x);
-	splay(pre), splay(suc, pre);
-	int u = t[suc].ch[0];
-	if (t[u].cnt > 1) t[u].cnt--, splay(u);
-	else t[suc].ch[0] = t[u].fa = 0;
-}
-/* 建spaly, 加入 $\pm \infty$ 的哨兵节点 */
-void init() {
-	root = idx = 0;
-	insert(-INTINF), insert(INTINF);
-	t[1].size = t[2].size = t[1].cnt = t[2].cnt = 0;
+void dfs(node *rt) {
+	if (rt == NIL) return;
+	rt -> PushDown();
+	dfs(rt -> ch[0]);
+	if (rt -> key != INF && rt -> key != -INF) {
+		if(ok) printf(" ");
+		ok = 1;
+		printf("%d",rt -> key);
+	}
+	dfs(rt -> ch[1]);
 }
