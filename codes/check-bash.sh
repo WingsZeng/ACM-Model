d2c202b8b2d25ad506633842fd1588b81b279c12
WingsZeng <wings.xiangyi.zeng@gmail.com>
2021-11-21 18:54:55 +0800

    更新传入种子的高级对拍

--- codes/check-bash.sh
+++ codes/check-bash.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
+let i=1
 while true; do
-    ./X-data > ./X.in
-    ./X < ./X.in > ./X.out
-    ./X-bf < ./X.in > ./X-bf.out
-    if diff ./X.out ./X-bf.out; then
-    	printf "AC\n"
+    echo $i | ./X-data > X.in
+    ./X < X.in > X.out
+    ./X-bf < X.in > X-bf.out
+    if diff X.out X-bf.out; then
+        echo $i AC
     else
-        printf "WA\n"
-        cat ./X.in
+        echo $i WA
+        cat X.in
         exit 0
     fi
+    let i=i+1
 done
