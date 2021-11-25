3fd32c950bddc44024458d3ba604817247bf6417
WingsZeng <wings.xiangyi.zeng@gmail.com>
2021-11-25 21:21:13 +0800

    修复树形背包模板存在的问题

--- codes/dp/tree-pack.cpp
+++ codes/dp/tree-pack.cpp
@@ -1,11 +1,13 @@
-void dfs(int u) {
+void dfs(int u, int f) {
 	sz[u] = 1;
-	dp[u][1] = a[u];
-	for (int v : G[u]) {
-		dfs(v);
-		for (int k = sz[u]; k; k--)
-			for (int x = 1; x <= sz[v]; x++)
-				dp[u][x+k] = max(dp[u][x+k], dp[u][k] + dp[v][x]);
-		sz[u] += sz[v];
+	dp[u][1] = a[u], dp[u][0] = 0;	// 初值
+	for (int v : G[u]) if (v != f){
+		dfs(v, u);
+		for (int i = 0; i <= sz[u] + sz[v]; i++) tmp[i] = 0;
+		for (int i = sz[u]; i >= 0; i--)    // 是否必选一个决定枚举到1还是0
+			for (int j = 0; j <= sz[v]; j++)// 同上
+				tmp[i+j] = max(tmp[i+j] dp[u][i] + dp[v][j]);
+		sz[u] += sz[v];		// 合并
+		for (int i = 0; i <= sz[u]; i++) dp[u][i] = tmp[i];
 	}
 }
