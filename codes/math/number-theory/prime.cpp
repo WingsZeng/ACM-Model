350472ec5cf26b288b0b0eeb0fd034a19d779400
WingsZeng <wings.xiangyi.zeng@gmail.com>
2022-06-25 18:17:20 +0800

    修改prime

--- codes/math/number-theory/prime.cpp
+++ codes/math/number-theory/prime.cpp
@@ -1,8 +1,9 @@
+int isnotprime[MAXN], prime[MAXN], p_cnt;
 void init_prime(int _n) {
-	isnotprime[0] = isnotprime[1] = 1; cnt = 0;
+	isnotprime[0] = isnotprime[1] = 1; p_cnt = 0;
 	for (int i = 2; i <= _n; i++) {
-		if (!isnotprime[i]) prime[cnt++] = i;
-		for (int j = 0; j < cnt && i * prime[j] <= _n; j++) {
+		if (!isnotprime[i]) prime[p_cnt++] = i;
+		for (int j = 0; j < p_cnt && i * prime[j] <= _n; j++) {
 			isnotprime[i * prime[j]] = 1;
 			if (!(i % prime[j])) break;
 		}
