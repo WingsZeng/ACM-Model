da8b3159e93bae5405d633a9e22c5dc458499d2a
WingsZeng <wings.xiangyi.zeng@gmail.com>
2022-07-07 14:28:44 +0800

    修正线性逆元代码

--- codes/math/number-theory/linear-inverse.cpp
+++ codes/math/number-theory/linear-inverse.cpp
@@ -1,4 +1,4 @@
-void init_liner_inverse() {
+void init_liner_inverse(int _n) {
 	inv[0] = inv[1] = 1;
-	for (int i = 2; i <= n; i++) inv[i] = mult(P - P / i, inv[P % i]);
+	for (int i = 2; i <= _n; i++) inv[i] = mult(P - P / i, inv[P % i]);
 }
